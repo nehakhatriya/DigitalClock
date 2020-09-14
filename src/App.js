4790ac9ed587b1e88d0438c2d09ea7716455fe19
nehakhatriya <nehakhatriya@gmail.com>
2020-09-14 17:04:16 +0530

    digital clock

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import classes from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = { toggle: false, date: new Date() }
+
+  toggleHandler = () => {
+
+    this.setState(prevstate => ({ toggle: !prevstate.toggle }))
+  }
+
+
+  render() {
+
+    let days = ['mon', 'tue', 'wed', 'thur', 'fri', 'sat', 'sun']
+    let months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+    let time = `${this.state.date.getHours()}:${this.state.date.getMinutes() < 10 ? '0' + this.state.date.getMinutes() : this.state.date.getMinutes()}:${this.state.date.getSeconds() < 10 ? '0' + this.state.date.getSeconds() : this.state.date.getSeconds()}`
+
+
+    let todaydate = this.state.toggle ?
+      `${days[this.state.date.getDay()]} ${this.state.date.getDate()} ${months[this.state.date.getMonth()]} ${this.state.date.getFullYear()}`
+      : null
+
+    return (
+      <div className={classes.clockheader}>
+        <header>
+          <h1>Digital Clock</h1>
+        </header>
+
+        <div className={classes.clock}>
+
+          <button className={classes.togglediv} type="" id="togglebtn" onClick={this.toggleHandler}><h4>DATE</h4></button>
+          <div className={classes.Clockdiv}>
+            <label id="time" className={classes.Time}>{time}</label>
+            <div>
+              <label id="date" className={classes.Date}>{todaydate}</label>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { configure,shallow } from 'enzyme';
 import App from './App';
+import Adapter from 'enzyme-adapter-react-16'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({adapter: new Adapter()})
+const app=shallow(<App/>)
+
+it('renders correctly', () => {
+  expect(app).toMatchSnapshot();
 });
+
+it('should display current time',()=>{
+  let date=new Date();
+  const min=date.getMinutes()<10?'0'+date.getMinutes():date.getMinutes()
+  const hour=date.getHours()
+  const sec=date.getSeconds()<10?'0'+date.getSeconds():date.getSeconds()
+  expect(app.find('#time').text()).toEqual(`${hour}:${min}:${sec}`)
+})
+
+it('should display date when toggle button is clicked',()=>{
+  let datee=new Date();
+  const days=['mon','tue','wed','thur','fri','sat','sun']
+  const day=days[datee.getDay()]
+  const today=datee.getDate()
+  const months=['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
+  const month=months[datee.getMonth()]
+  const year=datee.getFullYear()
+  app.find("#togglebtn").simulate('click')
+  expect(app.find('#date').text()).toEqual(`${day} ${today} ${month} ${year}`)
+})
